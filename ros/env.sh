c3e413484b7cfd8924fabb14ac38369a563cee3f
tam <tam@example.com>
2025-06-07 08:38:23 +0700

    Update

--- ros/env.sh
+++ ros/env.sh
@@ -6,8 +6,5 @@ source /opt/ros/melodic/setup.bash
 # If using a workspace (e.g., catkin_ws), source it
 source ~/catkin_ws/devel/setup.bash
 
-# Set ROS Master URI to itself (Jetson)
-export ROS_MASTER_URI=http://192.168.68.103:11311
-
-# Set Jetson's own IP address
-export ROS_HOSTNAME=192.168.68.100  # Replace with Jetson's actual IP address
+# Execute the passed command
+exec "$@"
